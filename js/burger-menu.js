c15096fc3ac17ee0c3a7639d6029d4dd4827a511
anaastassi <anaastassi@gmail.com>
2023-03-27 01:58:29 +0300

    fix problem in burger js

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,16 +1,16 @@
 (function () {
-    const header = document.querySelector('.header');
-    const btn = document.querySelector("header > div.navbar-toggler");
-
-    function changeBackground(){
-        if (btn.ariaExpanded === 'true'){
-            header.style.backgroundImage = 'none';
-            header.style.backgroundColor = '#493B5F' ;
-        }else{
-            header.style.backgroundImage = "url(img/header/header-background.png)";
-        }
+    const header = document.querySelector(".header");
+    const btn = document.querySelector(
+      "body > header > nav > div > div.navbar-toggler"
+    );
+  
+    function changeBackground() {
+      if (btn.ariaExpanded === "true") {
+        header.style.backgroundImage = "none";
+        header.style.backgroundColor = "#493B5F";
+      } else {
+        header.style.backgroundImage = "url(img/header/header-background.png)";
+      }
     }
-    btn.addEventListener('click', changeBackground);
-    overlay.addEventListener('click', function (event) {
-        if(event.target.tagName === "A") close();
-})();
+    btn.addEventListener("click", changeBackground);
+  })();
