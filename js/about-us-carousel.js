c9054f0d167f26009188e73811357a60fe85d262
fhctyw <lisnychukarsenw@gmail.com>
2023-03-31 21:24:21 +0300

    Apply Prettier 2.8.0 to all files

--- js/about-us-carousel.js
+++ js/about-us-carousel.js
@@ -1,48 +1,54 @@
 (function () {
+  const photoSlides = [
+    '<div class="worker"><img src="img/about-us-second-page/jane-woss.png" alt="Jane Woss"><p class="workers__name">Jane Woss</p></div></div>',
+    '<div class="worker"><img src="img/about-us-second-page/tomas-jackson.png" alt="Tomas Jackson"><p class="workers__name">Tomas Jackson</p></div></div>',
+    '<div class="worker"><img src="img/about-us-second-page/eloise-birkenstone.png" alt="Eloise Birkenstone"><p class="workers__name">Eloise Birkenstone</p></div></div>',
+  ];
 
-    const photoSlides = [
-        '<div class="worker"><img src="img/about-us-second-page/jane-woss.png" alt="Jane Woss"><p class="workers__name">Jane Woss</p></div></div>',
-        '<div class="worker"><img src="img/about-us-second-page/tomas-jackson.png" alt="Tomas Jackson"><p class="workers__name">Tomas Jackson</p></div></div>',
-        '<div class="worker"><img src="img/about-us-second-page/eloise-birkenstone.png" alt="Eloise Birkenstone"><p class="workers__name">Eloise Birkenstone</p></div></div>',
-    ];
+  const slideContainer = document.querySelector(
+    ".staff__carousel .staff__carousel__workers"
+  );
 
-    const slideContainer = document.querySelector('.staff__carousel .staff__carousel__workers');
+  let currentSlide = 0;
 
-    let currentSlide = 0;
-
-    function currentPhotos(photoSlides) {
-        slideContainer.innerHTML = photoSlides[currentSlide];
-        if (window.innerWidth > 500) {
-            const secondSlide = currentSlide + 1 >= photoSlides.length ? 0 : currentSlide + 1;
-            slideContainer.innerHTML += photoSlides[secondSlide]
-        }
+  function currentPhotos(photoSlides) {
+    slideContainer.innerHTML = photoSlides[currentSlide];
+    if (window.innerWidth > 500) {
+      const secondSlide =
+        currentSlide + 1 >= photoSlides.length ? 0 : currentSlide + 1;
+      slideContainer.innerHTML += photoSlides[secondSlide];
     }
+  }
 
-    function nextPhoto() {
-        currentSlide++;
-        if (currentSlide >= photoSlides.length) {
-            currentSlide = 0;
-        }
-        currentPhotos(photoSlides);
+  function nextPhoto() {
+    currentSlide++;
+    if (currentSlide >= photoSlides.length) {
+      currentSlide = 0;
     }
-
-    function prevPhoto() {
-        currentSlide--;
-        if (currentSlide < 0) {
-            currentSlide = photoSlides.length - 1;
-        }
-        currentPhotos(photoSlides);
-    }
-
     currentPhotos(photoSlides);
+  }
 
-    const nextButton = document.querySelector("div.staff__carousel > div.arrow-btn-next");
-    nextButton.addEventListener('click', nextPhoto);
+  function prevPhoto() {
+    currentSlide--;
+    if (currentSlide < 0) {
+      currentSlide = photoSlides.length - 1;
+    }
+    currentPhotos(photoSlides);
+  }
 
-    const prevButton = document.querySelector("div.staff__carousel > div.arrow-btn-prev");
-    prevButton.addEventListener('click', prevPhoto);
+  currentPhotos(photoSlides);
 
-    window.addEventListener('resize', () => {
-        currentPhotos(photoSlides);
-    });
+  const nextButton = document.querySelector(
+    "div.staff__carousel > div.arrow-btn-next"
+  );
+  nextButton.addEventListener("click", nextPhoto);
+
+  const prevButton = document.querySelector(
+    "div.staff__carousel > div.arrow-btn-prev"
+  );
+  prevButton.addEventListener("click", prevPhoto);
+
+  window.addEventListener("resize", () => {
+    currentPhotos(photoSlides);
+  });
 })();
